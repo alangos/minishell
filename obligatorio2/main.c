bda14b8daf7d3bbe0903a741c06e510f3a69c45c
Alan Gostanian <alangos@gmail.com>
2014-06-02 20:52:10 -0300

    Initial Commit

---
+++ obligatorio2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  obligatorio2
+//
+//  Created by Alan Gostanian on 02/06/14.
+//  Copyright (c) 2014 Alan Gostanian. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
